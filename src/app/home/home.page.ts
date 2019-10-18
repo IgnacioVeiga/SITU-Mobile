6890cc7872a649035efe6932f0249052fec0a5e5
Ignacio Veiga <ignacio.veiga29@gmail.com>
2019-10-18 01:35:54 -0300

    algunos ajustes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,10 @@ import { Router } from '@angular/router';
   templateUrl: 'home.page.html'
 })
 export class HomePage {
+  /*
+  esta es la pantalla de inicio,
+  tanto para pasajeros como choferes
+  */
 
   constructor(
     private router: Router
