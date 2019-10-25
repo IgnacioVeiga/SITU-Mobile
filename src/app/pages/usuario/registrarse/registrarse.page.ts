610437adfa4f97d95bf13f772db3a88783e2b4b9
Jamil Uribe <juribe@ubicuagroup.com>
2019-10-25 18:47:52 -0300

    Update ingresar and registrarse

--- src/app/pages/usuario/registrarse/registrarse.page.ts
+++ src/app/pages/usuario/registrarse/registrarse.page.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrarse',
   templateUrl: './registrarse.page.html'
 })
-export class RegistrarsePage implements OnInit {
+export class RegistrarsePage {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  goTo(path: string) {
+    this.router.navigate([path]);
   }
 
 }
