39fab1f9828a525823b909cfcd0acb94181aa1db
Jamil Uribe <juribe@ubicuagroup.com>
2019-10-26 18:56:51 -0300

    Update horarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
@@ -44,7 +44,8 @@ import { NoticiasAlertasPageModule } from './pages/noticias-alertas/noticias-ale
     StatusBar,
     SplashScreen,
     Geolocation,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AlertController
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/horarios/horarios.page.ts
+++ src/app/pages/horarios/horarios.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-horarios',
@@ -7,12 +8,57 @@ import { Router } from '@angular/router';
 })
 export class HorariosPage {
 
+  mostrarHorarios = false;
+
   constructor(
-    private router: Router
+    private router: Router,
+    public alertController: AlertController,
   ) { }
 
   goHome() {
     this.router.navigate(['inicio']);
+  }
+
+  cerrarMostrarHorarios() {
+    this.mostrarHorarios = false;
+  }
+
+  async presentAlertPrompt() {
+    this.mostrarHorarios = false;
+    const alert = await this.alertController.create({
+      header: 'Seleccione',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Línea'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          placeholder: 'Recorrido'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Mostrar paradas',
+          handler: () => {
+            this.mostrarHorarios = true;
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
